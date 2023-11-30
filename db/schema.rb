8d1de4a32b656f88b5264a41b4864b47d6a2317e
Markayel <mtorelus@gmail.com>
2023-11-30 10:44:17 -0800

    add story 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_224132) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_002003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "elephantidaes", force: :cascade do |t|
     t.string "common_name"
     t.string "scientific_binomial"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "animal_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
